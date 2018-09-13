aee70803ca4407a8c7d587a696797f3664f26817
edgar.sadler <edgar.sadler@CPX-6S1ROXTC39U>
2018-09-13 11:00:17 -0500

    test calcs

---
+++ TestCalculations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCalculations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCalculations")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf3407d1-34d4-4a96-8ae6-6a2ad3116458")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCalculations/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculations
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculator.CalculatorSoapClient calcRef;
+        [TestMethod]
+        public void TestAdd()
+        {
+
+            calcRef = new Calculator.CalculatorSoapClient();
+            double d = calcRef.Add(2.0, 2.0);
+            Assert.AreEqual(d, 4.0);
+        }
+        [TestMethod]
+        public void TestDivide()
+        {
+
+            calcRef = new Calculator.CalculatorSoapClient();
+            double d = calcRef.Divide(8.0, 2.0);
+            Assert.AreEqual(d, 4.0);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+
+            calcRef = new Calculator.CalculatorSoapClient();
+            double d = calcRef.Multiply(2.0, 2.0);
+            Assert.AreEqual(d, 4.0);
+        }
+
+        [TestMethod]
+        public void TestSubtrct()
+        {
+
+            calcRef = new Calculator.CalculatorSoapClient();
+            double d = calcRef.Subtrct(4.0, 2.0);
+            Assert.AreEqual(d, 2.0);
+        }
+
+    }
+}
